a2ec8daca40c43c7ef1d86fb223a0ae5c0b76f47
leonchik (Леонид) <leon.ru123@gmail.com>
2023-11-02 19:16:53 +0300

    create repo

---
+++ EventoryMVP/Pages/Equipment.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EventoryMVP.Pages
+{
+    public class EquipmentModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ EventoryMVP/Pages/Loft.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EventoryMVP.Pages
+{
+    public class LoftModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
