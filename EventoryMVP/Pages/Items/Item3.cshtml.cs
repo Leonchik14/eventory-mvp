94cf5edb9b90b100c6ff8f879b163e6e57bea5b8
Nikita Kitaev <nik_kitaev@mail.ru>
2023-11-07 21:15:38 +0300

    templates added

---
+++ EventoryMVP/Pages/Items/Item3.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EventoryMVP.Pages.Items
+{
+    public class Item3Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ EventoryMVP/Pages/Lofts/Loft3.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EventoryMVP.Pages.Lofts
+{
+    public class Loft3Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
